8311a93ba8b9a6e32cd7612b638d4bbf46d55564
louphier <louphier@gmail.com>
2020-04-23 23:15:14 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -10,5 +10,6 @@
 git clone https://github.com/frainzy1477/luci-app-clash package/luci-app-clash
 git clone https://github.com/vernesong/OpenClash.git package/luci-app-openclash
 git clone https://github.com/fw876/helloworld.git package/lean/luci-app-ssr-plus
+git clone https://github.com/tty228/luci-app-serverchan.git package/luci-app-serverchan
 ./scripts/feeds update -a
 ./scripts/feeds install -a
