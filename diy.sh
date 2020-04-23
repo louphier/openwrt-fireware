d6a093529af160a704e3cf1328511450c8b2565a
louphier <louphier@gmail.com>
2020-04-23 12:36:09 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -8,7 +8,7 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 git clone https://github.com/frainzy1477/luci-app-clash package/luci-app-clash
-git clone https://github.com/vernesong/OpenClash/tree/master/luci-app-openclash package/luci-app-clash
+git clone https://github.com/vernesong/OpenClash/tree/master/luci-app-openclash package/luci-app-openclash
 git clone https://github.com/fw876/helloworld.git /package/lean/luci-app-ssr-plus
 ./scripts/feeds update -a
 ./scripts/feeds install -a
